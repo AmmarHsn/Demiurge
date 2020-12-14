a79289885e6228ed3ba5bb22785f4391127a780e
Sobolevsky.A <Andrey.Sobolevsky@ulb.ac.be>
2020-12-14 09:56:02 +0100

    exploration

--- src/modules/AutoMoDeBehaviour.cpp
+++ src/modules/AutoMoDeBehaviour.cpp
@@ -10,56 +10,65 @@
 
 #include "AutoMoDeBehaviour.h"
 
-
-namespace argos {
+namespace argos
+{
 
 	AutoMoDeBehaviour::~AutoMoDeBehaviour() {}
 
 	/****************************************/
 	/****************************************/
 
-	const bool AutoMoDeBehaviour::IsLocked() const {
+	const bool AutoMoDeBehaviour::IsLocked() const
+	{
 		return m_bLocked;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeBehaviour::SetIndex(const UInt32& un_index) {
+	void AutoMoDeBehaviour::SetIndex(const UInt32 &un_index)
+	{
 		m_unIndex = un_index;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeBehaviour::GetIndex() const {
+	const UInt32 &AutoMoDeBehaviour::GetIndex() const
+	{
 		return m_unIndex;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeBehaviour::SetIdentifier(const UInt32& un_id) {
+	void AutoMoDeBehaviour::SetIdentifier(const UInt32 &un_id)
+	{
 		m_unIdentifier = un_id;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeBehaviour::GetIdentifier() const {
+	const UInt32 &AutoMoDeBehaviour::GetIdentifier() const
+	{
 		return m_unIdentifier;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const std::string AutoMoDeBehaviour::GetDOTDescription() {
+	const std::string AutoMoDeBehaviour::GetDOTDescription()
+	{
 		std::stringstream ss;
 		ss << m_strLabel;
-		if (!m_mapParameters.empty()) {
+		if (!m_mapParameters.empty())
+		{
 			std::map<std::string, Real>::iterator it;
-			for (it = m_mapParameters.begin(); it != m_mapParameters.end(); it++) {
-				ss << "\\n" << it->first << "=" << it->second ;
+			for (it = m_mapParameters.begin(); it != m_mapParameters.end(); it++)
+			{
+				ss << "\\n"
+				   << it->first << "=" << it->second;
 			}
 		}
 		return ss.str();
@@ -68,7 +77,8 @@ namespace argos {
 	/****************************************/
 	/****************************************/
 
-	CVector2 AutoMoDeBehaviour::ComputeWheelsVelocityFromVector(CVector2 c_vector_to_follow) {
+	CVector2 AutoMoDeBehaviour::ComputeWheelsVelocityFromVector(CVector2 c_vector_to_follow)
+	{
 		Real fLeftVelocity = 0;
 		Real fRightVelocity = 0;
 		CRange<CRadians> cLeftHemisphere(CRadians::ZERO, CRadians::PI);
@@ -84,11 +94,15 @@ namespace argos {
 		// 		fLeftVelocity = 1;
 		// 	}
 		// }
-		if (c_vector_to_follow.GetX() != 0 || c_vector_to_follow.GetY() != 0) {
-			if (cLeftHemisphere.WithinMinBoundExcludedMaxBoundExcluded(cNormalizedVectorToFollow)) {
+		if (c_vector_to_follow.GetX() != 0 || c_vector_to_follow.GetY() != 0)
+		{
+			if (cLeftHemisphere.WithinMinBoundExcludedMaxBoundExcluded(cNormalizedVectorToFollow))
+			{
 				fRightVelocity = 1;
 				fLeftVelocity = Cos(cNormalizedVectorToFollow);
-			} else {
+			}
+			else
+			{
 				fRightVelocity = Cos(cNormalizedVectorToFollow);
 				fLeftVelocity = 1;
 			}
@@ -104,9 +118,11 @@ namespace argos {
 	/****************************************/
 	/****************************************/
 
-	CVector2 AutoMoDeBehaviour::SumProximityReadings(CCI_EPuckProximitySensor::TReadings s_prox) {
+	CVector2 AutoMoDeBehaviour::SumProximityReadings(CCI_EPuckProximitySensor::TReadings s_prox)
+	{
 		CVector2 cSum(0, 0);
-		for (UInt8 i = 0; i < s_prox.size(); i++) {
+		for (UInt8 i = 0; i < s_prox.size(); i++)
+		{
 			cSum += CVector2(s_prox[i].Value, s_prox[i].Angle);
 		}
 		return cSum;
@@ -115,111 +131,230 @@ namespace argos {
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeBehaviour::AddParameter(const std::string& str_identifier, const Real& f_value) {
+	void AutoMoDeBehaviour::AddParameter(const std::string &str_identifier, const Real &f_value)
+	{
 		m_mapParameters.insert(std::pair<std::string, Real>(str_identifier, f_value));
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const bool AutoMoDeBehaviour::IsOperational() const {
+	const bool AutoMoDeBehaviour::IsOperational() const
+	{
 		return m_bOperational;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const std::string& AutoMoDeBehaviour::GetLabel() const {
+	const std::string &AutoMoDeBehaviour::GetLabel() const
+	{
 		return m_strLabel;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const std::map<std::string, Real> AutoMoDeBehaviour::GetParameters() {
+	const std::map<std::string, Real> AutoMoDeBehaviour::GetParameters()
+	{
 		return m_mapParameters;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeBehaviour::SetRobotDAO(EpuckDAO* pc_robot_dao) {
+	void AutoMoDeBehaviour::SetRobotDAO(EpuckDAO *pc_robot_dao)
+	{
 		m_pcRobotDAO = pc_robot_dao;
 	}
 
 	/****************************************/
-    /****************************************/
-    // Return the color parameter
-    CColor AutoMoDeBehaviour::GetColorParameter(const UInt32& un_value, const bool& b_emiter) {
+	/****************************************/
+	// Return the color parameter
+	CColor AutoMoDeBehaviour::GetColorParameter(const UInt32 &un_value, const bool &b_emiter)
+	{
 
-        CColor cColorParameter;
+		CColor cColorParameter;
 
-        //********************************************************
-        // HARD CODED true for real robots, false for simulation
-        // TODO Change for parameter in xml
+		//********************************************************
+		// HARD CODED true for real robots, false for simulation
+		// TODO Change for parameter in xml
 
-        bool b_real_robot = false;
+		bool b_real_robot = false;
 
-        //********************************************************
+		//********************************************************
 
-        if (b_real_robot && b_emiter){
-            switch(un_value){
-            case 0:
-                cColorParameter = CColor::BLACK;
-                break;
-            case 1:
-                cColorParameter = CColor::GREEN;
-                break;
-            case 2:
-                cColorParameter = CColor::BLUE;
-                break;
-            case 3:
-                cColorParameter = CColor::RED;
-                break;
-            case 4:
-                cColorParameter = CColor(250,80,0);
-                break;
-            case 5:
-                cColorParameter = CColor(150,0,60);
-                break;
-            case 6:
-                cColorParameter = CColor(0,200,50);
-                break;
-            default:
-                cColorParameter = CColor::BLACK;
-            }
-        }
-        else {
-            switch(un_value){
-            case 0:
-                cColorParameter = CColor::BLACK;
-                break;
-            case 1:
-                cColorParameter = CColor::GREEN;
-                break;
-            case 2:
-                cColorParameter = CColor::BLUE;
-                break;
-            case 3:
-                cColorParameter = CColor::RED;
-                break;
-            case 4:
-                cColorParameter = CColor::YELLOW;
-                break;
-            case 5:
-                cColorParameter = CColor::MAGENTA;
-                break;
-            case 6:
-                cColorParameter = CColor::CYAN;
-                break;
-            default:
-                cColorParameter = CColor::BLACK;
-            }
-        }
+		if (b_real_robot && b_emiter)
+		{
+			switch (un_value)
+			{
+			case 0:
+				cColorParameter = CColor::BLACK;
+				break;
+			case 1:
+				cColorParameter = CColor::GREEN;
+				break;
+			case 2:
+				cColorParameter = CColor::BLUE;
+				break;
+			case 3:
+				cColorParameter = CColor::RED;
+				break;
+			case 4:
+				cColorParameter = CColor(250, 80, 0);
+				break;
+			case 5:
+				cColorParameter = CColor(150, 0, 60);
+				break;
+			case 6:
+				cColorParameter = CColor(0, 200, 50);
+				break;
+			default:
+				cColorParameter = CColor::BLACK;
+			}
+		}
+		else
+		{
+			switch (un_value)
+			{
+			case 0:
+				cColorParameter = CColor::BLACK;
+				break;
+			case 1:
+				cColorParameter = CColor::GREEN;
+				break;
+			case 2:
+				cColorParameter = CColor::BLUE;
+				break;
+			case 3:
+				cColorParameter = CColor::RED;
+				break;
+			case 4:
+				cColorParameter = CColor::YELLOW;
+				break;
+			case 5:
+				cColorParameter = CColor::MAGENTA;
+				break;
+			case 6:
+				cColorParameter = CColor::CYAN;
+				break;
+			default:
+				cColorParameter = CColor::BLACK;
+			}
+		}
 
-        return cColorParameter;
+		return cColorParameter;
 	}
 
-	
+	/****************************************/
+	/****************************************/
 
-}
+	CVector2 AutoMoDeBehaviour::GetHeadingVector(CVector2 sCustomVector, UInt32 m_proxyFactor)
+	{
+
+		CVector2 sProxVector = CVector2(m_pcRobotDAO->GetProximityReading().Value, m_pcRobotDAO->GetProximityReading().Angle);
+		CVector2 sResultVector = sCustomVector - m_proxyFactor * sProxVector;
+
+		if (sResultVector.Length() < m_fProximityThreshold)
+		{
+			sResultVector = CVector2(1, CRadians::ZERO);
+		}
+		if (sCustomVector.Length() < m_fProximityThreshold)
+		{
+			switch (m_ExplorationType)
+			{
+			case 0:
+				// Ballistic motion with obstacle avoidance
+				break;
+			case 1:
+				// Ballistic motion with random turn
+				UpdateRandomTurn();
+				sResultVector = CVector2(1, m_walkAngle);
+				break;
+			case 2:
+				// Random walk
+				UpdateRandomWalk();
+				sResultVector = CVector2(1, m_walkAngle);
+				break;
+			default:
+				break;
+			}
+		}
+		return sResultVector;
+	}
+
+	void AutoMoDeBehaviour::UpdateRandomTurn()
+	{
+		if (m_walkSteps <= 0)
+		{
+			if (IsObstacleInFront(m_pcRobotDAO->GetProximityReading()))
+			{
+				m_walkSteps = (m_pcRobotDAO->GetRandomNumberGenerator())->Uniform(m_cRandomStepsRange);
+				CRadians cAngle = m_pcRobotDAO->GetProximityReading().Angle.SignedNormalize();
+				if (cAngle.GetValue() < 0)
+				{
+					m_walkAngle = CRadians::PI - CRadians(0.0001);
+				}
+				else
+				{
+					m_walkAngle = CRadians::PI + CRadians(0.0001);
+				}
+			}
+			else
+			{
+				m_walkAngle = CRadians::ZERO;
+			}
+		}
+		else
+		{
+			m_walkSteps -= 1;
+		}
+	}
+
+	void AutoMoDeBehaviour::UpdateRandomWalk()
+	{
+		if (m_walkSteps <= 0)
+		{
+
+			CRange<Real> range;
+			range.SetMin(0.0);
+			range.SetMax(1.0);
+			Real r = (m_pcRobotDAO->GetRandomNumberGenerator())->Uniform(range);
+			Real bias = 0.0;
+			Real lMin = 1.0;
+			m_walkSteps = ceil(lMin * pow(r, 1 / (1 - mu)) + 0.5);
+			m_walkAngle = CRadians(2 * atan(((1 - rho) / (1 + rho)) * tan(M_PI * (r - 0.5))) + bias).SignedNormalize();
+		}
+		else
+		{
+			m_walkSteps -= 1;
+		}
+
+		if (AutoMoDeBehaviour::IsObstacleInFront(m_pcRobotDAO->GetProximityReading()))
+		{
+			CRadians obstacleAngle = m_pcRobotDAO->GetProximityReading().Angle.SignedNormalize();
+
+			if (obstacleAngle >= CRadians::ZERO)
+			{
+				obstacleAngle -= CRadians::PI;
+			}
+			else
+			{
+				obstacleAngle += CRadians::PI;
+			}
+
+			m_walkAngle = (m_walkAngle + obstacleAngle) / 2;
+		}
+	}
+
+	bool AutoMoDeBehaviour::IsObstacleInFront(CCI_EPuckProximitySensor::SReading s_prox_reading)
+	{
+		CRadians cAngle = s_prox_reading.Angle;
+		if (s_prox_reading.Value >= m_fProximityThreshold && ((cAngle <= CRadians::PI_OVER_TWO) && (cAngle >= -CRadians::PI_OVER_TWO)))
+		{
+			return true;
+		}
+		return false;
+	}
+
+} // namespace argos
--- src/modules/AutoMoDeBehaviour.h
+++ src/modules/AutoMoDeBehaviour.h
@@ -22,158 +22,176 @@
 
 #include <map>
 
-namespace argos {
-	class AutoMoDeBehaviour {
-		protected:
-			/*
+namespace argos
+{
+	class AutoMoDeBehaviour
+	{
+	protected:
+		/*
 			 * Tells whether or not the behaviour is locked for a given
 			 * number of steps. No conditions should be tested if it is locked.
 			 */
-			bool m_bLocked;
+		bool m_bLocked;
 
-			/*
+		/*
 			 * Tells whether or not the behaviour is ready to be executed.
 			 * This could happen if a previous reseting step takes more than
 			 * one time step.
 			 */
-			bool m_bOperational;
+		bool m_bOperational;
 
-			/*
+		/*
 			 * Contains the parameters of the behaviours.
 			 */
-			std::map<std::string, Real> m_mapParameters;
+		std::map<std::string, Real> m_mapParameters;
 
-			/*
+		/*
 			 * The name of the behaviour.
 			 */
-			std::string m_strLabel;
+		std::string m_strLabel;
 
-			/*
+		/*
 			 * The index of the behaviour in the FSM behaviour list.
 			 */
-			UInt32 m_unIndex;
+		UInt32 m_unIndex;
 
-			/*
+		/*
 			 * The identifier of the behaviour.
 			 */
-			UInt32 m_unIdentifier;
+		UInt32 m_unIdentifier;
 
-			/*
+		/*
 			 * Pointer to the state of the robot. Shared with the controller AutoMoDeController
 			 * and the finite state machine AutoMoDeFiniteStateMachine.
 			 */
-      EpuckDAO* m_pcRobotDAO;
+		EpuckDAO *m_pcRobotDAO;
 
-		public:
+		Real m_fProximityThreshold;
 
-		 virtual ~AutoMoDeBehaviour();
-			/*
+		UInt8 m_ExplorationType;
+		SInt32 m_walkSteps;
+		CRadians m_walkAngle;
+
+		Real mu;
+		Real rho;
+		CRange<UInt32> m_cRandomStepsRange;
+
+	public:
+		virtual ~AutoMoDeBehaviour();
+		/*
 			 * Execution of the behaviour. Based on the state of the robot,
 			 * the behaviour should update the output variables.
 			 */
-			virtual void ControlStep() = 0;
+		virtual void ControlStep() = 0;
 
-			/*
+		/*
 			 * Method used to reset the status (variables) of the behaviour.
 			 */
-			virtual void Reset() = 0;
+		virtual void Reset() = 0;
 
-			/*
+		/*
 			 * Method called at the reseting of the behaviour when more than one
 			 * time step are needed to prepare before the execution of the behaviour
 			 * (that is the call of ControlStep).
 			 */
-			virtual void ResumeStep() = 0;
+		virtual void ResumeStep() = 0;
 
-			/*
+		/*
 			 * Initialize the behaviour.
 			 */
-			virtual void Init() = 0;
+		virtual void Init() = 0;
 
-			/*
+		/*
 			 * Cloning function.
 			 */
-			virtual AutoMoDeBehaviour* Clone() = 0;
+		virtual AutoMoDeBehaviour *Clone() = 0;
 
-			/*
+		/*
 			 * Returns a string containing the DOT description of the behaviour.
 			 */
-			const std::string GetDOTDescription();
+		const std::string GetDOTDescription();
 
-			/*
+		/*
 			 * Instert a pair <parameter, value> to the parameters map.
 			 */
-			void AddParameter(const std::string& str_identifier, const Real& f_value);
+		void AddParameter(const std::string &str_identifier, const Real &f_value);
 
-			/*
+		/*
 			 * Returns the value of a given parameter from the parameters map.
 			 */
-			const Real& GetParameter(const std::string& str_identifier);
+		const Real &GetParameter(const std::string &str_identifier);
 
-			/*
+		/*
 			 * Returns the whole parameters map.
 			 */
-			const std::map<std::string, Real> GetParameters();
+		const std::map<std::string, Real> GetParameters();
 
-			/*
+		/*
 			 * Setter for the index of the behaviour.
 			 */
-			void SetIndex(const UInt32& un_index);
+		void SetIndex(const UInt32 &un_index);
 
-			/*
+		/*
 			 * Getter for the index of the behaviour.
 			 */
-			const UInt32& GetIndex() const;
+		const UInt32 &GetIndex() const;
 
-			/*
+		/*
 			 * Setter for the identifier of the behaviour.
 			 */
-			void SetIdentifier(const UInt32& un_id);
+		void SetIdentifier(const UInt32 &un_id);
 
-			/*
+		/*
 			 * Getter for the identifier of the behaviour.
 			 */
-			const UInt32& GetIdentifier() const;
+		const UInt32 &GetIdentifier() const;
 
-			/*
+		/*
 			 * Getter for the label (name) of the behaviour.
 			 */
-			const std::string& GetLabel() const;
+		const std::string &GetLabel() const;
 
-
-			/*
+		/*
 			 * Returns the value of the locked flag.
 			 * @see m_bLocked.
 			 */
-			const bool IsLocked() const;
+		const bool IsLocked() const;
 
-			/*
+		/*
 			 * Returns the value of the operational flag.
 			 * @see m_bOperational;
 			 */
-			const bool IsOperational() const;
+		const bool IsOperational() const;
 
-			/*
+		/*
 			 * Utility function. Returns a vector containing the wheels velocity
 			 * needed for the robot to follow the vector passed as parameter of the method.
 			 */
-			CVector2 ComputeWheelsVelocityFromVector(CVector2 c_vector_to_follow);
+		CVector2 ComputeWheelsVelocityFromVector(CVector2 c_vector_to_follow);
 
-			/*
+		/*
 			 * Utility function. Returns a vector containing the sum of the
 			 * proximity readings passed as parameter of the method.
 			 */
-			CVector2 SumProximityReadings(CCI_EPuckProximitySensor::TReadings s_prox);
+		CVector2 SumProximityReadings(CCI_EPuckProximitySensor::TReadings s_prox);
 
-			/*
+		/*
 			 * Setter for the shared pointer to the representation of the robot state.
 			 */
-			void SetRobotDAO(EpuckDAO* pc_robot_dao);
-			/*
+		void SetRobotDAO(EpuckDAO *pc_robot_dao);
+		/*
              * Data transform for color of the omnidirectional camera and LEDs.
              */
-            CColor GetColorParameter(const UInt32& un_value, const bool& b_emiter);
+		CColor GetColorParameter(const UInt32 &un_value, const bool &b_emiter);
+
+		void UpdateRandomWalk();
+
+		void UpdateRandomTurn();
+
+		CVector2 GetHeadingVector(CVector2 sCustomVector, UInt32 m_proxyFactor);
+
+		bool IsObstacleInFront(CCI_EPuckProximitySensor::SReading s_prox_reading);
 	};
-}
+} // namespace argos
 
 #endif
